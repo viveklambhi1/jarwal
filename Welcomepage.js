dda5966cadaed0ec4d7382694f932aba80f45615
naveenj92 <58801277+naveenj92@users.noreply.github.com>
2019-12-23 18:04:14 +0530

    Update Welcomepage.js

--- Welcomepage.js
+++ Welcomepage.js
@@ -4,64 +4,86 @@ import {
   Text,
   KeyboardAvoidingView,
   TouchableOpacity,
-  View
+  View,
+  Image
 } from 'react-native';
-
 import {
   Card,
   CardImage,
   CardTitle,
   CardContent,
   CardAction,
-
 } from 'react-native-card-view';
-import { StackNavigator } from 'react-navigation';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
 import Button from 'react-native-button';
+import Header from '../src/header';
 
 export default class Welcomepage extends Component {
+  static navigationOptions = {
+     header: null,
+  };
 
     render () {
-		return(
-        <Card>
-          <CardTitle>
-            <Text style={{fontSize:50, color: 'black',left:140,alignItems: 'center',top:190}}>WELCOME</Text>
-            <Text style={{fontSize:30, color: 'black',top:260,alignItems: 'center',right:120}}>Employee Name Here</Text>
-          </CardTitle>
+      const context=this;
+       var userObj = JSON.parse(context.props.navigation.state.params.userObj);
+       console.log(userObj.success.user.employee.profile_picture);
+        const {navigate} = this.props.navigation;
+        var profile_picture={uri:userObj.success.user.employee.profile_picture};
 
+		return(
+
+           <Card>
+           <Header/>
+            <CardImage>
+           <Image source={profile_picture} style={{bottom:70,height:150,width:150,borderRadius:75,borderColor:'black'}}/>
+            </CardImage>
+          <CardTitle>
+            <Text style={{fontSize:50, color: 'black',alignItems: 'center',top:35,textAlignVertical:'center',textAlign:'right'}}>WELCOME</Text>
+              </CardTitle>
+               <CardTitle>
+            <Text style={{fontSize:30, color: 'black',bottom:0,alignItems: 'center',textAlignVertical:'center',textAlign:'right'}}>{userObj.success.user.employee.fullname}</Text>
+               </CardTitle>
+          <CardAction >
             <Button
               style={styles.button1}
-              onPress={() => {}}>
+              onPress={() => navigate('cameraPage')}>
               Check In
             </Button>
+
             <Button
               style={styles.button2}
-              onPress={() => {}}>
+              onPress={() => navigate('cameraPage')}>
               Check Out
             </Button>
-
-        </Card>
-
+              </CardAction >
+              </Card>
       );
     }
   }
 
+
   const styles = StyleSheet.create({
 
     button1: {
-      flexDirection: 'row',
+
       flex:0,
       margin: 0,
       color: '#DCE4EF',
-      top: -68,
-      left: -100,
+      left: 0,
       alignItems: 'center',
+      justifyContent: 'center',
+      zIndex: 100,
+      marginBottom:180,
+      marginRight:10,
       paddingTop:23,
       paddingBottom:23,
       paddingLeft:50,
       paddingRight:50,
       backgroundColor:'rgb(19,111,232)',
       borderRadius:10,
-      borderWidth: 2,
+      borderWidth: 1,
       borderColor: 'rgb(0,0,0)',
     },
     button2: {
@@ -72,17 +94,18 @@ export default class Welcomepage extends Component {
       marginLeft:0,
       marginRight:0,
       marginBottom:0,
-        right:-100,
+        right:0,
         color: '#DCE4EF',
-        top: -142,
         alignItems: 'center',
+        marginBottom:180,
+        marginLeft:10,
         paddingTop:23,
         paddingBottom:23,
         paddingLeft:50,
         paddingRight:50,
         backgroundColor:'rgb(19,111,232)',
         borderRadius:10,
-        borderWidth: 2,
+        borderWidth: 1,
         borderColor: 'rgb(0,0,0)',
 
       },
