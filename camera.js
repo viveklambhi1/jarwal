c111c96b208644ea577dc06ba48e2f3f04eb1a0a
naveenj92 <58801277+naveenj92@users.noreply.github.com>
2019-12-24 18:24:09 +0530

    Update camera.js

--- camera.js
+++ camera.js
@@ -20,6 +20,8 @@ import {
   CardContent,
   CardAction,
 } from 'react-native-card-view';
+import { Marker } from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, PROVIDER_DEFAULT} from 'react-native-maps';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import CommentBoxInput from './CommentBoxInput';
@@ -46,6 +48,31 @@ export async function request_camera_runtime_permission() {
     console.warn(err)
   }
 }
+export async function request_ACCESS_FINE_LOCATION_runtime_permission() {
+
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        'title': 'XENIA LOCATION Permission',
+        'message': 'XENIA App needs access to your LOCATION '
+      }
+    )
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      Alert.alert("LOCATION Permission Granted.");
+    }
+    else {
+      Alert.alert("LOCATION Permission Not Granted");
+    }
+  } catch (err) {
+    console.warn(err)
+  }
+}
+export const getCurrentLocation = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
+  });
+};
 export default class App extends React.Component {
   static navigationOptions = {
      header: null,
@@ -54,11 +81,14 @@ export default class App extends React.Component {
   await request_camera_runtime_permission()
 }
 
+
   constructor(props) {
     super(props)
     this.state = {
-      file:'',
+      file:"http://erpportaltest.xeamventures.com/public/uploads/profile-pics/1563432304.png",
       comment:'',
+      latitude: '30.715260',
+      longitude:'76.707770',
     };
     this.selectPhotoTapped = this.selectPhotoTapped.bind(this);
   }
@@ -81,7 +111,7 @@ export default class App extends React.Component {
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton);
       } else {
-        let source = {uri: response.uri};
+        let source = {uri:response.uri};
         // You can also display the image using data:
         // let source = { uri: 'data:image/jpeg;base64,' + response.data };
         this.setState({
@@ -90,32 +120,53 @@ export default class App extends React.Component {
       }
     });
   }
+  componentDidMount() {
+    return getCurrentLocation().then(position => {
+      if (position) {
+        this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.003,
+            longitudeDelta: 0.003,
+        });
+      }
+    });
+  }
+  async componentDidMount() {
+  await request_ACCESS_FINE_LOCATION_runtime_permission()
+  }
    submit = () =>{
-     const {file,comment} = this.state;
+     const {file,comment,longitude,latitude} = this.state;
      console.log("I am going to call login api")
       var data = JSON.stringify({
-        file:this.state.file,
-        comment:this.state.comment,
+          latitude:this.state.latitude,
+          longitude:this.state.longitude,
+          comment:this.state.comment,
+          file:this.state.file,
       })
      console.log(data);
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
 xhr.addEventListener("readystatechange", function () {
-if (this.readyState === 4) {
+  console.log(xhr.status)
+  console.log(xhr.readyState)
+  if (xhr.readyState !== 4) {
+              return;
+  }
+  if (xhr.status === 200) {
+    console.log("Successfully200")
+  }else{
+    console.log("inside error")
+  }
   console.log(this.responseText);
-}
-if (xhr.status === 200) {
-  console.log("Successfully200")
-  // context.props.navigation.navigate("welcome");
-}else{
-  console.log("inside error")
-   alert(xhr.responseText);
-}
+  alert(this.responseText);
+
+
 });
 xhr.open("POST", "http://erpportaltest.xeamventures.com/api/v1/attendance-location");
 xhr.setRequestHeader("Accept", "application/json");
 xhr.setRequestHeader("Content-Type", "application/javascript");
-xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjkyYTg4NTcwYjA3YTc4NjU3M2YwZmI5MjcyYWQzNGU5ZTMxZTMyMjVhYmNkMmMzNzVkYmE3MzY1YmJmNzI1ZjM3MWM1ZGMzOTNmOTY2ODBlIn0.eyJhdWQiOiIzIiwianRpIjoiOTJhODg1NzBiMDdhNzg2NTczZjBmYjkyNzJhZDM0ZTllMzFlMzIyNWFiY2QyYzM3NWRiYTczNjViYmY3MjVmMzcxYzVkYzM5M2Y5NjY4MGUiLCJpYXQiOjE1NzcwOTkwOTcsIm5iZiI6MTU3NzA5OTA5NywiZXhwIjoxNjA4NzIxNDk3LCJzdWIiOiIxMDYiLCJzY29wZXMiOltdfQ.eNc8WnncNEuDcRrWa1VE33JgRTuhjdRWK0iKjCW_B7KaEHVRDHlrlsGvAx87-yhmTRZuKDV8s6PNnGROOjsa3QUoEuno2iBayQcO71P2AGiQt5rV1JsiMFRqkC2xn3rtAtf0eqmZcgyObxyXOjwq_1LbaBaRleJvD0Fthf7foqP0Wdp782N06IOAz53Mpf1HoroWemmp0dTFKRtmzd51Fr82jbNMs75eyb3hO-9Bb5eRJtI2Intmu9ZNlHkGXlR2zvqwCoSf-X8SBgLaF8F6PO3Gw27-yaXIihUbKYN6_vJjuRyUKrWnr2WJevKQOtZNO7hnFgfYjoPuN2V00Ydu4sfphf6aNXLQ2O-8EhviKvA6MCgJWtML_cwFh3kPOKeeM8j2zHAMf-BE1US45Txs-8BBE70koezE11reWZxfE3FOv1t0q568FbWGOViPeb24zkCp7bzNnn48LuPMwjE9_Iuen_gJLiKFkBQJRsvK6bJFYmK5Subgz60W_9qdu209OprRS6WS-nW_8hhgdUrklT2KTOmw0GVMIQ_Bi8i9U92xjBjA8hvn0ImDXvlEOA13snM9ffgn7garhWV4YNXxPwjwLkzv-aA6es0FVjXI_DOuzUQMxOafv03g79kx3y7yv1EjAziNrIEMpRd9PLPXhXBb4Iv1tVApOSkqPFNVEes");
+xhr.setRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImVlYTkwNjlhMjM3NmNkZTY3OWY5OWZhYjU1OWVkOTY3NmE5Y2M5MTY3ODRhYjUwYTY5NDc3ZjZjN2U0ODhlYTY3NzU5OTI2NTgwZTdiNjg2In0.eyJhdWQiOiIzIiwianRpIjoiZWVhOTA2OWEyMzc2Y2RlNjc5Zjk5ZmFiNTU5ZWQ5Njc2YTljYzkxNjc4NGFiNTBhNjk0NzdmNmM3ZTQ4OGVhNjc3NTk5MjY1ODBlN2I2ODYiLCJpYXQiOjE1NzcxODQ0MzYsIm5iZiI6MTU3NzE4NDQzNiwiZXhwIjoxNjA4ODA2ODM2LCJzdWIiOiIxMDYiLCJzY29wZXMiOltdfQ.qcX9DC2k7VNPyVcgg5nw81DR9j0waoeTG9oasGXEWHlApKvMDuPk1_BpPqm-9GYLFwZdgfm7fxwUl7Ry16VnVxrj0WnkBsrI4J3ZSpgI5izngw4cdTW93FWZzEwfy5kW_fgvJCLxOT4ZJL-nSbkTusjXd-zTa1SeWvX7svgoWduQHYphe_1-aog0xDzDS-uEnD4ZXS1yWHU_mEEFWFxzxtJwsJl8hoUJZUDep__i2cjvcGCTj9orFoSjjj8SZvq08OLLyijrhk5YtM55l_9320H8O1VqCRZVLiJChFNbtfaI2NykUfClGnv0SfZZOBYShDtAx9op-CmS-OdwkkeHhB030BySXtcJv8qFl0xWmo2SMTCZqlXqYpGVn3vRcNgxGKB_vu-kRG56m5LCIpi49njMJWiX5BnN8Jzv1H61DnsKNTLGdgYYbiTJeKptiiW0vzUfnyMI2xc7WuPvFua2HzPDMPM6BitRu3aE3ATCbDh45BTu4IFdiLV4R25kKY8QI7ucESa2XLbfER5j2Gbt8tGNFy92MVzpTwfvUe0tlCcOggniDQ5cZpEXkEFT9nZaQdj1V-Tl-EcnA7bXhTHicu2Z-jjsUFTiAOAEautEF2K4yY_SM4NJF-DlGaEGe7vx2w9Mc0Qoelsgx3Sb9rtVo-tc7ToAxqsrAv5mNrfKsaI");
 xhr.send(data);
    }
 
@@ -136,7 +187,26 @@ xhr.send(data);
             )}
             </View>
           </TouchableOpacity>
-          <View><Text style={{fontSize:50, top:50}}>MAP VIEW</Text></View>
+          <View style={styles.MainContainer}>
+            <MapView
+              style={styles.mapStyle}
+              provider={PROVIDER_GOOGLE}
+              showsUserLocation={true}
+              zoomEnabled={true}
+              zoomControlEnabled={true}
+              Region={{
+                latitude: 30.715260,
+                longitude: 76.707770,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}>
+              <Marker
+                coordinate={{ latitude:30.715260, longitude:76.707770 }}
+                title={""}
+                description={""}
+              />
+            </MapView>
+          </View>
           <CommentBoxInput
             placeholder="COMMENT BOX"
             autoCapitalize={'done'}
@@ -184,7 +254,34 @@ const styles = StyleSheet.create({
   },
   avatar: {
     borderRadius: 10,
+    borderTopWidth: 1,
+    borderBottomWidth:1,
+    borderRightWidth:1,
+    borderLeftWidth:1,
     width:400,
     height: 150,
+  },
+  MainContainer: {
+    borderRadius: 8,
+    borderTopWidth: 1,
+    borderBottomWidth:1,
+    borderRightWidth:1,
+    borderLeftWidth:1,
+    position: 'absolute',
+    height: 200,
+    width: 400,
+    marginBottom: 200,
+    left: 50,
+    right: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  mapStyle: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
