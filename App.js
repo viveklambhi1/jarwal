9b001b4796475317a00f39a0ce94270761840413
naveenj92 <58801277+naveenj92@users.noreply.github.com>
2019-12-31 13:46:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -17,12 +17,14 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import LoginPage from './src/LoginPage';
 import Welcomepage from './src/Welcomepage';
+import CameraPage from './src/CameraPage';
 
 
 
   const MainNavigator = createStackNavigator({
   login: {screen: LoginPage},
   welcome: {screen: Welcomepage},
+  cameraPage: {screen: CameraPage},
 });
 
 const App = createAppContainer(MainNavigator);
